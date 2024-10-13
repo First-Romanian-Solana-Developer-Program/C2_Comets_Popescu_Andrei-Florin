5c70dd458f97e0a828bb71a18c3e128fabf6d198
Popescu Andrei-Florin <andrei.popescu2811@stud.acs.upb.ro>
2024-10-13 18:35:28 +0300

    init lab1

---
+++ lab1/load-keypair.ts
@@ -0,0 +1,8 @@
+import "dotenv/config";
+import { getKeypairFromEnvironment } from "@solana-developers/helpers";
+
+const keypair = getKeypairFromEnvironment("SECRET_KEY");
+
+console.log('Loaded keypair! ✅');
+console.log('Public key:' + keypair.publicKey.toBase58());
+console.log('Private key:' + keypair.secretKey);
